32470561ab9218f25e4ea66e9d0ac2483bc71619
Diego Telles <diego.telles@samisaude.com>
2022-09-23 20:50:53 -0300

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React, { InputHTMLAttributes } from 'react';
+
+export interface OwnProps extends InputHTMLAttributes<HTMLButtonElement> {
+  label: string;
+}
+
+const ButtonWrapper = (props: OwnProps) => {
+  return (
+    <div>
+      <button>{props.label}</button>
+    </div>
+  );
+};
+
+export default ButtonWrapper;
